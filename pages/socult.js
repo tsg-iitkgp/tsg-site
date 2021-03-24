c701afd71b47507cb24a9121466e9a68c659ed9c
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-25 03:08:34 +0530

    added a chart with dummy data; removed society list;

--- pages/socult.js
+++ pages/socult.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import { ResponsiveBar } from "@nivo/bar";
+import pointsData from "../public/pointsData";
 
 export default function Socult() {
   return (
@@ -17,17 +19,102 @@ export default function Socult() {
           is Yoga". Our goal is to bring overall development in IITians through cultivating and nurturing their
           extra-curricular talents.
         </p>
-        <p>
-          <h3> Societies and Clubs</h3>
-          <ul>
-            <li>Eastern Technology Music Society</li>
-            <li>Western Technology Music Soceity</li>
-            <li>Technology Dance Society</li>
-            <li>Spectra</li>
-            <li>Encore</li>
-            <li>Pravah</li>
-          </ul>
-        </p>
+        <div className="chart">
+          <h2> Points Tally GC 18-19</h2>
+          <ResponsiveBar
+            data={pointsData.data}
+            keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+            indexBy="country"
+            margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+            padding={0.3}
+            valueScale={{ type: "linear" }}
+            indexScale={{ type: "band", round: true }}
+            colors={{ scheme: "nivo" }}
+            defs={[
+              {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "#38bcb2",
+                size: 4,
+                padding: 1,
+                stagger: true,
+              },
+              {
+                id: "lines",
+                type: "patternLines",
+                background: "inherit",
+                color: "#eed312",
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10,
+              },
+            ]}
+            fill={[
+              {
+                match: {
+                  id: "fries",
+                },
+                id: "dots",
+              },
+              {
+                match: {
+                  id: "sandwich",
+                },
+                id: "lines",
+              },
+            ]}
+            borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "country",
+              legendPosition: "middle",
+              legendOffset: 32,
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "food",
+              legendPosition: "middle",
+              legendOffset: -40,
+            }}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+            legends={[
+              {
+                dataFrom: "keys",
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: "left-to-right",
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+          />
+        </div>
       </section>
     </Layout>
   );
