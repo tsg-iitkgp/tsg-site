19966aed829b8f56fcc2b2b17b09a06635755b9c
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-03-25 16:55:23 +0530

    added dropdowns to mobile navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import Link from "next/link";
+import * as Icon from "react-feather";
 
 export default function Navbar() {
   const [isActive, setIsActive] = useState(false);
+  const [isDown_1, setIsDown_1] = useState(false);
+  const [isDown_2, setIsDown_2] = useState(false);
 
   const navbarNikal = () => {
     isActive ? setIsActive(false) : setIsActive(true);
+  };
+
+  const dropdownNikal_1 = () => {
+    isDown_1 ? setIsDown_1(false) : setIsDown_1(true);
+  };
+
+  const dropdownNikal_2 = () => {
+    isDown_2 ? setIsDown_2(false) : setIsDown_2(true);
   };
 
   return (
@@ -84,26 +95,44 @@ export default function Navbar() {
               {/* <li data-text="Updates">
                 <Link href="#">Updates</Link>
               </li> */}
-              <li data-text="Swimming Pool">
-                <Link href="/pool">Swimming Pool</Link>
+              <li
+                data-text="Facilities"
+                className={`dropdown-title ${isDown_1 ? "down" : ""}`}
+                onClick={dropdownNikal_1}
+              >
+                Facilities <Icon.ChevronDown />
+                <ul className="dropdown-content">
+                  <li data-text="Swimming Pool">
+                    <Link href="/pool">Swimming Pool</Link>
+                  </li>
+                  <li data-text="Tennis Court">
+                    <Link href="/court">Tennis Court</Link>
+                  </li>
+                  <li data-text="Jnan Ghosh Stadium">
+                    <Link href="/stadium">Jnan Ghosh Stadium</Link>
+                  </li>
+                  <li data-text="Tagore Open Air Theater">
+                    <Link href="/toat">Tagore Open Air Theater</Link>
+                  </li>
+                </ul>
               </li>
-              <li data-text="Tennis Court">
-                <Link href="/court">Tennis Court</Link>
-              </li>
-              <li data-text="Jnan Ghosh Stadium">
-                <Link href="/stadium">Jnan Ghosh Stadium</Link>
-              </li>
-              <li data-text="Tagore Open Air Theater">
-                <Link href="/toat">Tagore Open Air Theater</Link>
-              </li>
-              <li data-text="Sports &amp; Games">
-                <Link href="/sports">Sports &amp; Games</Link>
-              </li>
-              <li data-text="Social &amp; Cultural">
-                <Link href="/socult">Social &amp; Cultural</Link>
-              </li>
-              <li data-text="Technology">
-                <Link href="/tech">Technology</Link>
+              <li
+                data-text="Hall of Fame"
+                className={`dropdown-title ${isDown_2 ? "down" : ""}`}
+                onClick={dropdownNikal_2}
+              >
+                Hall of Fame <Icon.ChevronDown />
+                <ul className="dropdown-content">
+                  <li data-text="Sports &amp; Games">
+                    <Link href="/sports">Sports &amp; Games</Link>
+                  </li>
+                  <li data-text="Social &amp; Cultural">
+                    <Link href="/socult">Social &amp; Cultural</Link>
+                  </li>
+                  <li data-text="Technology">
+                    <Link href="/tech">Technology</Link>
+                  </li>
+                </ul>
               </li>
               <li data-text="Societies">
                 <Link href="/societies">Societies</Link>
