3391445299630734e670e479c5a73e3ebe835953
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-04-18 17:07:05 +0530

    removed unnecessary code from footer.js and _footer.scss;

--- components/footer.js
+++ components/footer.js
@@ -1,64 +1,78 @@
-import Link from "next/link";
-
 export default function Footer() {
   return (
     <section className="footer">
-      <p>
-        <ul className="events">
-          <li className="footer-block">
-            TSG Gymkhana
-            <p>
-              Technology Students' Gymkhana <br /> IIT Kharagpur, Kharagpur <br />
-              West Bengal - 721302
-            </p>
-          </li>
-          <li className="footer-block">
-            Quick Links
-            <ul>
-              <li>
-                <Link href="http://www.iitkgp.ac.in/">IITKGP Website</Link>
-              </li>
-              <li>
-                <Link href="https://erp.iitkgp.ernet.in/">ERP</Link>
-              </li>
-              <li>
-                <Link href="https://iitkgpmail.iitkgp.ac.in/">Zimbra webmail</Link>
-              </li>
-              <li>
-                <Link href="http://www.iitkgp.ac.in/iitkgp-announcements/">Annoucements</Link>
-              </li>
-              <li>
-                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform">
-                  Complaint Form
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.facebook.com/TSG.IITKharagpur/">TSG Facebook</Link>
-              </li>
-              <li>
-                <Link href="https://twitter.com/tsg_iitkgp?lang=en">TSG Twitter</Link>
-              </li>
-            </ul>
-          </li>
-          <li className="footer-block">
-            Quick Links
-            <ul>
-              <li>
-                <Link href="https://wiki.metakgp.org/w/Yellow_pages/">Your Dost</Link>
-              </li>
-              <li>
-                <Link href="http://www.hmc.iitkgp.ac.in/web/">Hall Management Centre</Link>
-              </li>
-              <li>
-                <Link href="https://wiki.metakgp.org/w/Yellow_pages">Yellow Pages</Link>
-              </li>
-              <li>
-                <Link href="http://www.counsellingcentre.iitkgp.ac.in/">Counselling Centre</Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </p>
+      <ul>
+        <li>
+          Contact Us
+          <p>
+            Technology Students' Gymkhana <br /> IIT Kharagpur, Kharagpur <br />
+            West Bengal - 721302
+          </p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="http://www.iitkgp.ac.in/" target="_blank" rel="noopener noreferrer">
+            IITKGP Website
+          </a>
+        </li>
+        <li>
+          <a href="https://erp.iitkgp.ernet.in/" target="_blank" rel="noopener noreferrer">
+            ERP
+          </a>
+        </li>
+        <li>
+          <a href="https://iitkgpmail.iitkgp.ac.in/" target="_blank" rel="noopener noreferrer">
+            Zimbra webmail
+          </a>
+        </li>
+        <li>
+          <a href="http://www.iitkgp.ac.in/iitkgp-announcements/" target="_blank" rel="noopener noreferrer">
+            Annoucements
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Complaint Form
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/TSG.IITKharagpur/" target="_blank" rel="noopener noreferrer">
+            TSG Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/tsg_iitkgp?lang=en" target="_blank" rel="noopener noreferrer">
+            TSG Twitter
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="https://wiki.metakgp.org/w/Yellow_pages/" target="_blank" rel="noopener noreferrer">
+            Your Dost
+          </a>
+        </li>
+        <li>
+          <a href="http://www.hmc.iitkgp.ac.in/web/" target="_blank" rel="noopener noreferrer">
+            Hall Management Centre
+          </a>
+        </li>
+        <li>
+          <a href="https://wiki.metakgp.org/w/Yellow_pages" target="_blank" rel="noopener noreferrer">
+            Yellow Pages
+          </a>
+        </li>
+        <li>
+          <a href="http://www.counsellingcentre.iitkgp.ac.in/" target="_blank" rel="noopener noreferrer">
+            Counselling Centre
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
