10f4468738c9f33f68fce3e9d9e96c0df881ea66
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-04-26 13:30:59 +0530

    updated mobile navbar with newer enteries;

--- components/navbar.js
+++ components/navbar.js
@@ -6,8 +6,8 @@ import * as Icon from "react-feather";
 export default function Navbar() {
   const [isActive, setIsActive] = useState(false);
   const [isDown_1, setIsDown_1] = useState(false);
-  // const [isDown_2, setIsDown_2] = useState(false);
-  // const [isDown_3, setIsDown_3] = useState(false);
+  const [isDown_2, setIsDown_2] = useState(false);
+  const [isDown_3, setIsDown_3] = useState(false);
 
   const router = useRouter();
 
@@ -43,7 +43,7 @@ export default function Navbar() {
             )}
           </div>
           <ul>
-          <li data-text="about" className="dropdown-title">
+            <li data-text="about" className="dropdown-title">
               About <Icon.ChevronDown />
               <ul className="dropdown-content">
                 <li>
@@ -142,7 +142,30 @@ export default function Navbar() {
         <div className={`wrapper ${isActive ? "active" : ""}`}>
           <ul>
             <li data-text="GC" className={`dropdown-title ${isDown_1 ? "down" : ""}`} onClick={() => dropdownNikal(1)}>
-              Updates {isDown_1 ? <Icon.ChevronLeft /> : <Icon.ChevronRight />}
+              About {isDown_1 ? <Icon.ChevronLeft /> : <Icon.ChevronRight />}
+              <ul className="dropdown-content">
+                <li>
+                  <a href="/societies">Societies</a>
+                </li>
+                <li>
+                  <a href="/facilities">Facilities</a>
+                </li>
+                <li>
+                  <a href="/interIIT">Inter IIT</a>
+                </li>
+                <li>
+                  <a href="/gc">General Championship</a>
+                </li>
+                <li>
+                  <a href="/openIIT">Open IIT</a>
+                </li>
+                <li>
+                  <a href="/staff">TSG Staff</a>
+                </li>
+              </ul>
+            </li>
+            <li data-text="GC" className={`dropdown-title ${isDown_2 ? "down" : ""}`} onClick={() => dropdownNikal(2)}>
+              Updates {isDown_2 ? <Icon.ChevronLeft /> : <Icon.ChevronRight />}
               <ul className="dropdown-content">
                 <li>
                   <a href="/gc">GC Results</a>
@@ -153,11 +176,34 @@ export default function Navbar() {
                 <li>OpenIIT Results</li>
               </ul>
             </li>
+            <li data-text="GC" className={`dropdown-title ${isDown_3 ? "down" : ""}`} onClick={() => dropdownNikal(3)}>
+              Links {isDown_3 ? <Icon.ChevronLeft /> : <Icon.ChevronRight />}
+              <ul className="dropdown-content">
+                <li>
+                  <a
+                    href="https://drive.google.com/file/d/1V1oppLrE3tC-wdB_vw3w3SgEYKLzb2_y/view?usp=sharing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    TSG Constitution &thinsp; <Icon.ExternalLink />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Complaint Form &thinsp; <Icon.ExternalLink />
+                  </a>
+                </li>
+                <li>
+                  <a href="/pastOfficeBearers">Past Office Bearers</a>
+                </li>
+              </ul>
+            </li>
             <li data-text="Hall of Fame">
               <Link href="/hallOfFame">Hall of Fame</Link>
-            </li>
-            <li data-text="Societies">
-              <Link href="/societies">Societies</Link>
             </li>
             <li data-text="Blog">
               <a href="https://tsg-blog.medium.com/" target="_blank" rel="noopener noreferrer">
