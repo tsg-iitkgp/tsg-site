e3446ec68aaec5803c9056300b4816caf63f4287
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-06-12 01:25:09 +0530

    past office bearers by posts rather than year

--- pages/pastOfficeBearers.js
+++ pages/pastOfficeBearers.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Head from "next/head";
 import Layout from "../components/layout";
-import senateData from "../public/senateData";
+import senateData from "../public/senateData.json";
 
-const years = ["2020", "2019", "2018", "2017", "2016"];
+const posts = ["VP", "GSecSports", "GSecTech", "GSecSocult", "GSecWelfare"];
 
 export default function Sports() {
-  const [currentYear, setCurrentYear] = useState("2020");
+  const [currentPost, setCurrentPost] = useState("VP");
 
   return (
     <Layout>
@@ -16,44 +16,21 @@ export default function Sports() {
       <section className="office">
         <h2> Past Office Bearers</h2>
 
-        <select value={currentYear} onChange={(e) => setCurrentYear(e.target.value)}>
-          {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
+        <select value={currentPost} onChange={(e) => setCurrentPost(e.target.value)}>
+          {posts.map((post) => (
+            <option key={post} value={post}>
+              {post}
             </option>
           ))}
         </select>
 
         {/* Now render based on the value of the currentYear */}
-        {senateData.data
-          .filter((obj) => obj.year == currentYear)
-          .map((item) => (
-            <div>
-              <div className="member">
-                <h2>Vice President:</h2>
-                <h3>{item.vp}</h3>
-              </div>
-              <div className="member">
-                <h2> General Secretary Sports:</h2>
-                <h3> {item.gsecsSports} </h3>
-              </div>
-
-              <div className="member">
-                <h2> General Secretary Social and Cultural:</h2>
-                <h3> {item.gsecsSocult} </h3>
-              </div>
-
-              <div className="member">
-                <h2>General Secretary Technology:</h2>
-                <h3>{item.gsecsTech}</h3>
-              </div>
-
-              <div className="member">
-                <h2>General Secretary Students' Welfare:</h2>
-                <h3>{item.gsecsStudentwelfare}</h3>
-              </div>
-            </div>
-          ))}
+        {senateData[currentPost].map((item) => (
+          <div className="member">
+            <h3>{item.year}</h3>
+            <h3>{item.name}</h3>
+          </div>
+        ))}
       </section>
     </Layout>
   );
