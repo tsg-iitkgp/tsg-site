edeb8f54642f5765830d4a0287d9884dbe0493db
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-06-13 21:38:53 +0530

    fixed about page; added new facilities image; aligned dropdown chevron;

--- pages/about.js
+++ pages/about.js
@@ -35,12 +35,12 @@ export default function About() {
             <h2>Facilities and Events</h2>
             <div className="image-modals">
               <div className="column">
-                {facilitiesData.data.slice(0, 3).map((facility) => (
+                {facilitiesData.data.slice(0, 5).map((facility) => (
                   <ImageModal images={facility.images} description={facility.description} title={facility.title} />
                 ))}
               </div>
               <div className="column">
-                {facilitiesData.data.slice(4, 7).map((facility) => (
+                {facilitiesData.data.slice(5, 10).map((facility) => (
                   <ImageModal images={facility.images} description={facility.description} title={facility.title} />
                 ))}
               </div>
