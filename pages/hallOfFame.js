99f5529cb757bf3aa6152cce74d46a4fde5a9450
Mukul Mehta <mukul.mehta@oracle.com>
2021-06-20 15:15:07 +0530

    Change default year for awards to 2018-19

--- pages/hallOfFame.js
+++ pages/hallOfFame.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout";
 import awardsData from "../public/awardsData.json";
 import Card from "../components/contactCard";
 
-const years = ["2018-19","2017-18", "2016-17", "2015-16"];
+const years = ["2018-19", "2017-18", "2016-17", "2015-16"];
 
 export default function Fame() {
   const [currentTab, setCurrentTab] = useState("sports");
-  const [currentYear, setCurrentYear] = useState("2015-16");
+  const [currentYear, setCurrentYear] = useState("2018-19");
 
   const handleTabChange = (s) => {
     setCurrentTab(s);
