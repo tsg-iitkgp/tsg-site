6415bc62ae8d125df79f909cb11486dd8083677f
Mukul Mehta <mukul.mehta@oracle.com>
2021-06-20 23:43:14 +0530

    Remove full contacts sheet from contacts page

--- pages/contacts.js
+++ pages/contacts.js
@@ -42,13 +42,6 @@ export default function Contacts() {
           })}
         </div>
       </section>
-      <div className="tabs">
-        <div className="tab">
-          <a href="/all_office_bearers.pdf" target="_blank" rel="noopener noreferrer">
-            TSG Team 2020-21 &thinsp; <Icon.ExternalLink />
-          </a>
-        </div>
-      </div>
     </Layout>
   );
 }
