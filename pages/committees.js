0515c82132bf53085dcdec93b3413136b66d0923
Mukul Mehta <mukul.mehta@oracle.com>
2021-06-24 23:40:54 +0530

    committee work

--- pages/committees.js
+++ pages/committees.js
@@ -1,37 +1,74 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-
-import facilitiesData from "../public/facilities";
-import ImageModal from "../components/imageModal";
-import Card from "../components/societyCard";
-import societiesData from "../public/societiesData";
-import festData from "../public/festData";
+import Layout from "../components/layout";
+import Card from "../components/committeeCard";
+import committeesData from "../public/committeesData";
+import Router from "next/router";
 
 export default function Committees() {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
+    <Layout>
+      <Head>
+        <title>Committees</title>
+      </Head>
+      <section className="societies content">
+        <h1>Committees</h1>
+        <h2>Social &amp; Cultural Societies</h2>
+        <div className="society-cards">
+          {committeesData.data
+            .filter((a) => a.category === "Socult")
+            .map((society) => {
+              return (
+                <Card
+                  name={society.name}
+                  facebook_link={society.facebook_link}
+                  wiki_link={society.wiki_link}
+                  shortform={society.shortform}
+                  description={society.description}
+                  Heads={society.Heads}
+                  website={society.website}
+                />
+              );
+            })}
+        </div>
 
-        <section className="m-content">
-          <section className="about" id="about">
-            <h1>About Us</h1>
-            <p>
-              Technology Students’ Gymkhana is the hub of the numerous extra-curricular and co-curricular activities in
-              IIT Kharagpur ranging from sports to socio-cultural. The Gymkhana is managed by the students, for the
-              students, under the guidance and active participation of the faculty and staff members.{" "}
-            </p>
-            <blockquote>"Yogah Karmasu Kausalam"</blockquote>
-            <p>
-              The moto of Technology Students' Gymkhana is YOGAH KARMASU KAUSALAM which in English means "Perfection in
-              action is Yoga". Our goal is to bring overall development in IITians through cultivating and nurturing
-              their extra-curricular talents.
-            </p>
-          </section>
-        </section>
-      </Layout>
-    </>
+        <h2>Tech Societies</h2>
+        <div className="society-cards">
+          {committeesData.data
+            .filter((a) => a.category === "Tech")
+            .map((society) => {
+              return (
+                <Card
+                  name={society.name}
+                  facebook_link={society.facebook_link}
+                  wiki_link={society.wiki_link}
+                  shortform={society.shortform}
+                  description={society.description}
+                  Heads={society.Heads}
+                  website={society.website}
+                />
+              );
+            })}
+        </div>
+
+        <h2>Sports &amp; Games Societies</h2>
+        <div className="society-cards">
+          {committeesData.data
+            .filter((a) => a.category === "Sports & Games")
+            .map((society) => {
+              return (
+                <Card
+                  name={society.name}
+                  facebook_link={society.facebook_link}
+                  wiki_link={society.wiki_link}
+                  shortform={society.shortform}
+                  description={society.description}
+                  Heads={society.Heads}
+                  website={society.website}
+                />
+              );
+            })}
+        </div>
+      </section>
+    </Layout>
   );
 }
