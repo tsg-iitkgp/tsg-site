e1583d9f0ae253cd7f17daa41d30696c3aa5043c
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-10-07 19:43:20 +0530

    Removal of expired events

--- pages/index.js
+++ pages/index.js
@@ -46,22 +46,6 @@ export default function Home() {
             <h2> Upcoming Events</h2>
             <AutoplaySlider play={true} interval={5000}>
               <div
-                data-src="/upcomingEvents/pw.png"
-                onClick={() => handlePosterClick("https://docs.google.com/forms/d/e/1FAIpQLSdpA3ObUAhBJqcyaczg0bzuRgMLYnQs_t3LwNgt2D0P-tymyA/viewform")}
-              />
-              <div
-                data-src="/upcomingEvents/ps.jpeg"
-                onClick={() => handlePosterClick("https://forms.gle/pCvGZqWgBjz6hU5j9")}
-              />
-              <div
-                data-src="/upcomingEvents/gssrs.jpeg"
-                onClick={() => handlePosterClick("https://youtu.be/-tO9zOQnWuQ")}
-              />
-              <div
-                data-src="/upcomingEvents/fob.jpeg"
-                onClick={() => handlePosterClick("https://docs.google.com/forms/d/e/1FAIpQLSeVydLePkKfuUu7lV2TbK2hHPw0XYVM_GwCnHO_e_XV4Bstuw/viewform?pli=1")}
-              />
-              <div
                 data-src="/upcomingEvents/fp.jpeg"
                 onClick={() => handlePosterClick("https://forms.gle/cFtth3ZoSJHwKP4w7")}
               />
