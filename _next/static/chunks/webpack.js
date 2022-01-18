830534aa93ec6afc8f8a642d0c112c2c5b0123a3
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-01-19 01:45:04 +0530

    Added Samyog page

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9233a30ad7da558a68e2";
+/******/ 	var hotCurrentHash = "a182aaefd31916132b03";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
