2b1cd0a5380c05d25964caa468e31c1ad22539e2
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-02-06 14:51:52 +0530

    PPC2022 banner added

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3ad68b5ba48bc448c91";
+/******/ 	var hotCurrentHash = "090e72c99486b8b1f413";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
