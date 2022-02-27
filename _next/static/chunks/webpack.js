836fcbac25532425cfd27942eef1abc1034419d2
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-02-27 11:26:37 +0530

    Match scoreboards addition

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b817089239d28999cad9";
+/******/ 	var hotCurrentHash = "6b3b0009e4829ba881d5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
