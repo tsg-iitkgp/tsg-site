2c3a04acd728fdcc6f269b94456e994bdc4066f2
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-03-06 13:17:13 +0530

    Added 2 more highlights

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4d9800ef68618e920bac";
+/******/ 	var hotCurrentHash = "20f0d07dec0e30b01ab2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
