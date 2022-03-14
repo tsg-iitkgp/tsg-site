548296f696f43952ff5e46fa84f9f05fc8fa92c1
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-03-14 18:24:09 +0530

    Added TT Scores and Elections page

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d42d028082ae95c3ab15";
+/******/ 	var hotCurrentHash = "fa73108527b7717da142";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
