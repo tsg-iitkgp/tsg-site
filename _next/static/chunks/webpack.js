4869bdfa01f33c2593a3b562c056300d6f078b4f
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-03-20 05:46:17 +0530

    Hockey symbol update

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3d59f9d8b0f7ae6f3885";
+/******/ 	var hotCurrentHash = "e4ce33e124590368b3fd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
