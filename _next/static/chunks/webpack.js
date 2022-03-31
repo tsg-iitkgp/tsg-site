e205c84eaa1e4ba1e2a9437eb14b18cc7307ce9d
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-03-31 20:42:19 +0530

    SOAP Schedule Update

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bc37127cc9b4359cc14";
+/******/ 	var hotCurrentHash = "d597aaa6e3ee4cafcde3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
